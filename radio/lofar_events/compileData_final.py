cd5179eaae6a6727030cb01f5ba7fb475fd75ae8
Katie <kmurley@gmail.com>
2020-02-17 23:10:58 +0100

    .

--- radio/lofar_events/compileData_final.py
+++ radio/lofar_events/compileData_final.py
@@ -52,6 +52,7 @@ print 'processing {0} files'.format(len(file_list))
 
 for i in np.arange(len(file_list)):
     if '2020' in file_list[i]:
+        print i
         #print file_list[i]
         infile=open(file_list[i],'r')
         info=cPickle.load(infile)
